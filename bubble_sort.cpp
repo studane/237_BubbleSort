33f8826a97ffca6e9c6e36b8bc6d9457ee55eb67
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-03 10:56:56 +0700

    Third Commit

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -6,4 +6,13 @@ int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n;       // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
 
 void input()
-{}
+{ // procedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : "; //Output ke layar
+        cin >> n;                                          //Input dari pengguna
+        if (n <=20)                                        //Jika n kurang dari atau sama dengan 20
+            break;
+
+    }}

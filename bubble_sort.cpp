2e87ef4df661677208812641e586d0f07fbe24b9
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-03 10:52:17 +0700

    Second Commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n;       // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
+void input()
+{}
